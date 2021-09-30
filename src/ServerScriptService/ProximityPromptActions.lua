9ead74ddbd755b289ddc0090651b3355195b6779
JaxxonJargonGames <jaxxonjargon@gmail.com>
2021-09-30 11:48:24 -0700

    Teleport from benches to rooftops.

--- src/ServerScriptService/ProximityPromptActions.lua
+++ src/ServerScriptService/ProximityPromptActions.lua
@@ -13,11 +13,21 @@ local function cloneWeaponToBackpack(toolName, player)
 end
 
 function module.promptTriggeredActions(promptObject, player)
-	local toolName = promptObject.Parent.Parent.Name
-	-- print("ProximityPromptActions - promptTriggeredActions:", toolName)
-	-- print("    promptObject.Enabled:", promptObject.Enabled)
-	WeaponUpgrades.upgradeTool(player, toolName)
-	promptObject.Enabled = false
+	if promptObject.Parent.Parent.Name == "TeleportBench_1" then
+		player.Character.Humanoid.RootPart.CFrame = workspace.Building_1.Spawn.CFrame
+	elseif promptObject.Parent.Parent.Name == "TeleportBench_2" then
+		player.Character.Humanoid.RootPart.CFrame = workspace.Building_2.Spawn.CFrame
+	elseif promptObject.Parent.Parent.Name == "TeleportBench_3" then
+		player.Character.Humanoid.RootPart.CFrame = workspace.Building_3.Spawn.CFrame
+	elseif promptObject.Parent.Parent.Name == "TeleportBench_4" then
+		player.Character.Humanoid.RootPart.CFrame = workspace.Building_4.Spawn.CFrame
+	else
+		local toolName = promptObject.Parent.Parent.Name
+		-- print("ProximityPromptActions - promptTriggeredActions:", toolName)
+		-- print("    promptObject.Enabled:", promptObject.Enabled)
+		WeaponUpgrades.upgradeTool(player, toolName)
+		promptObject.Enabled = false
+	end
 end
 
 return module
