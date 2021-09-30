1ef420ff07be0e4b4d0f72f111d7003904ea68b3
JaxxonJargonGames <jaxxonjargon@gmail.com>
2021-09-30 15:27:37 -0700

    Moved game.Players.CharacterAutoLoads to its rightful script location.

--- src/ServerScriptService/PlayerEvents.server.lua
+++ src/ServerScriptService/PlayerEvents.server.lua
@@ -1,3 +1,5 @@
+game.Players.CharacterAutoLoads = false
+
 game.Players.PlayerAdded:Connect(function(player)
 	player.CharacterAdded:Connect(function(character)
 		local humanoid = character:WaitForChild("Humanoid")
