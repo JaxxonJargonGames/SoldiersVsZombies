2494a22f141a21fd37f22f93adbeaa6883901144
JaxxonJargonGames <jaxxonjargon@gmail.com>
2021-10-05 21:00:38 -0700

    Creator only.

--- src/ServerScriptService/ResetEvent.server.lua
+++ src/ServerScriptService/ResetEvent.server.lua
@@ -8,8 +8,10 @@ local STARTING_POINTS = 0
 local event = Instance.new("RemoteEvent")
 event.Name = "ResetEvent"
 event.OnServerEvent:Connect(function(player)
-	player.leaderstats.Points.Value = STARTING_POINTS
-	WeaponUpgrades.reset()
-	player.Character.Humanoid.Health = 0
+	if player.UserId == game.CreatorId then
+		player.leaderstats.Points.Value = STARTING_POINTS
+		WeaponUpgrades.reset()
+		player.Character.Humanoid.Health = 0
+	end
 end)
 event.Parent = ReplicatedStorage
