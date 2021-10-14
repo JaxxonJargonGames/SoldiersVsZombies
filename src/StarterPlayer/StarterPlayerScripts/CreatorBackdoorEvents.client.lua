2bbc71dfe80350c94ed5e6fefbf55a6bcdb81ec4
JaxxonJargonGames <jaxxonjargon@gmail.com>
2021-10-14 15:13:03 -0700

    Gave event handlers specific names.

--- src/StarterPlayer/StarterPlayerScripts/CreatorBackdoorEvents.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/CreatorBackdoorEvents.client.lua
@@ -9,7 +9,7 @@ local ACTION_NAME = "Reset"
 local CREATE_TOUCH_BUTTON = false
 local INPUT_TYPES = Enum.KeyCode.Delete
 
-local function onEvent(actionName, inputState, inputObject)
+local function onResetEvent(actionName, inputState, inputObject)
 	if inputState == Enum.UserInputState.Begin then
 		if actionName == ACTION_NAME then
 			Event:FireServer(player)
@@ -17,14 +17,14 @@ local function onEvent(actionName, inputState, inputObject)
 	end
 end
 
-ContextActionService:BindAction(ACTION_NAME, onEvent, CREATE_TOUCH_BUTTON, INPUT_TYPES)
+ContextActionService:BindAction(ACTION_NAME, onResetEvent, CREATE_TOUCH_BUTTON, INPUT_TYPES)
 
 local Event = ReplicatedStorage:WaitForChild("Stage2BackdoorEvent")
 local ACTION_NAME = "Stage2Backdoor"
 local CREATE_TOUCH_BUTTON = false
 local INPUT_TYPES = Enum.KeyCode.Insert
 
-local function onEvent(actionName, inputState, inputObject)
+local function onStage2BackdoorEvent(actionName, inputState, inputObject)
 	if inputState == Enum.UserInputState.Begin then
 		if actionName == ACTION_NAME then
 			Event:FireServer(player)
@@ -32,14 +32,14 @@ local function onEvent(actionName, inputState, inputObject)
 	end
 end
 
-ContextActionService:BindAction(ACTION_NAME, onEvent, CREATE_TOUCH_BUTTON, INPUT_TYPES)
+ContextActionService:BindAction(ACTION_NAME, onStage2BackdoorEvent, CREATE_TOUCH_BUTTON, INPUT_TYPES)
 
 local Event = ReplicatedStorage:WaitForChild("WeaponUpgradesEvent")
 local ACTION_NAME = "WeaponUpgrades"
 local CREATE_TOUCH_BUTTON = false
 local INPUT_TYPES = Enum.KeyCode.U
 
-local function onEvent(actionName, inputState, inputObject)
+local function onWeaponUpgradesEvent(actionName, inputState, inputObject)
 	if inputState == Enum.UserInputState.Begin then
 		if actionName == ACTION_NAME then
 			Event:FireServer(player)
@@ -47,4 +47,4 @@ local function onEvent(actionName, inputState, inputObject)
 	end
 end
 
-ContextActionService:BindAction(ACTION_NAME, onEvent, CREATE_TOUCH_BUTTON, INPUT_TYPES)
+ContextActionService:BindAction(ACTION_NAME, onWeaponUpgradesEvent, CREATE_TOUCH_BUTTON, INPUT_TYPES)
