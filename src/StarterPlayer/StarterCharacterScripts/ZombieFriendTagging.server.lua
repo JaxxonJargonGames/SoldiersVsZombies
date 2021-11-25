5b5cc4b322bec641c7bd8cd112b589e073e644aa
JaxxonJargonGames <jaxxonjargon@gmail.com>
2021-11-25 09:58:45 -0800

    Tag every character as a zombie friend.

--- src/StarterPlayer/StarterCharacterScripts/ZombieFriendTagging.server.lua
+++ src/StarterPlayer/StarterCharacterScripts/ZombieFriendTagging.server.lua
@@ -1,26 +1,5 @@
 local CollectionService = game:GetService("CollectionService")
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
 local character = script.Parent
-local player = Players:GetPlayerFromCharacter(character)
 
-local isFriend = false
-
-print("Not Zombie Friend!")
-
-local event = Instance.new("RemoteEvent")
-event.Name = "ZombieFriendToggleEvent"
-event.OnServerEvent:Connect(function(player)
-	if player.UserId == game.CreatorId then
-		isFriend = not isFriend
-		if isFriend then
-			print("Zombie Friend!")
-			CollectionService:AddTag(character, "ZombieFriend")
-		else
-			print("Not Zombie Friend!")
-			CollectionService:RemoveTag(character, "ZombieFriend")
-		end
-	end
-end)
-event.Parent = ReplicatedStorage
+print("Zombie Friend!")
+CollectionService:AddTag(character, "ZombieFriend")
